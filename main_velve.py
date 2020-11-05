f0f2c4772efb25ab586cb4e7186af53c8eacfa32
Kristjan Axelsson <kristjan.axelsson@blickfeld.com>
2020-11-05 23:24:27 +0100

    added simulation layout

--- main_velve.py
+++ main_velve.py
@@ -15,7 +15,78 @@ plots = [
     PlotTubes()
 ]
 
-def backpressure():
+def leakage_current():
+
+    running_params = dict(
+
+        Pr_in=0,  # reservoirdruck
+        Pr_out=0,  # reservoirdruck
+        Pc0=0,  # startdruck in der pumpkammer
+        T=1e-6,  # simulationsdauer
+        steps=500,  # anzahl der zeitschritte
+
+        pump=dict(
+        ),
+        signal=dict(
+            amplitude=1e5,
+            frequency=1000e3,
+            offset=0,
+            a=1,
+        ),
+        velve_in=dict(
+            R_open=2e6,
+            R_close=1e11,
+            direction='forward',
+            #direction='backward',
+        ),
+        velve_out=dict(
+            R_open=2e6,
+            R_close=1e15,
+            #direction='forward',
+            direction='backward',
+        ),
+        tube_in=dict(
+            diameter=1e-3,
+            length=100e-3
+        ),
+        tube_out=dict(
+            diameter=1e-3,
+            length=100e-3
+        )
+    )
+
+    params = ParameterManager(running_params)
+
+    param_range = np.linspace(0, 1e5, 10) / 1
+
+    chamber_pressure = dict()
+    for new_param in param_range:
+
+        # params.components.tube_in.length += new_param
+        # params.components.signal.frequency = new_param
+        params.parameter.Pr_in = new_param
+
+        components, parameter = params()
+        time, y_data = velve_test(**components, **parameter)
+        chamber_pressure.update({f'{new_param:.4f}':y_data['chamber']})
+
+        pm = PlotManager()
+        pm.plot_dict(time, y_data,
+                     title='Simple Pump',
+                     xlabel='Time [s]',
+                     ylabel='Voltage [V]',
+                     filename=f'backpressure/{new_param:.4f}')
+
+    chamber_pressure.update({'signal': y_data['signal']})
+    pm.plot_dict(time, chamber_pressure,
+                 title='Backpressure',
+                 xlabel='Time [s]',
+                 ylabel='Chamber pressure [Pa]',
+                 filename=f'backpressure/pressure_sweep')
+
+
+
+def tube_length():
 
     running_params = dict(
 
