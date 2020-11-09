26fb47c68c294159e8641308d5a95917ba3d5927
Kristjan Axelsson <kristjan.axelsson@blickfeld.com>
2020-11-09 18:02:58 +0100

    created plots

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class System(SystemManager):
     Pr_out = 0  # reservoirdruck
     Pc0 = 0  # startdruck in der pumpkammer
     T = 1e-3  # simulationsdauer
-    steps = 1000  # anzahl der zeitschritte
+    steps = 100  # anzahl der zeitschritte
 
     class signal:
         _comp = Rectangle
@@ -66,35 +66,64 @@ def backpressure():
     vevel_out geht bei 50 kPa nicht mehr auf
     """
 
+    amplitude_in = dict()
+    amplitude_out = dict()
+
+    pm = PlotManager()
+
     system = System()
 
-    external_pressure = np.linspace(0, 0.5e5, 10)
+    external_pressure = np.linspace(0, 0.6e5, 31)
 
     chamber_pressure = dict()
 
-    for pressure in external_pressure:
+    for i, pressure in enumerate(external_pressure):
+
+
 
         # system.tube_in.length = new_param
         system.Pr_in = pressure
         system.Pr_out = -pressure
 
+        pres_label = pressure//1000
+
         components = system.get_components()
         parameter = system.get_parameter()
         time, y_data = system_test(**components, **parameter)
 
-        chamber_pressure.update({f'{pressure:.4f}':y_data['chamber']})
+        if not i:
+            chamber_pressure.update({'signal': y_data['signal_voltage']})
 
-        pm = PlotManager()
-        pm.plot_dict(time*1e3, y_data,
-                     title='',
-                     xlabel='Time [ms]',
-                     ylabel='Pressure [#]',
-                     filename=f'backpressure/{pressure:.4f}')
+        chamber_pressure.update({f'{pres_label} kPa':y_data['chamber']})
 
-    chamber_pressure.update({'signal_voltage': y_data['signal_voltage']})
+        amplitude_in[pres_label] = max(y_data['chamber'])
+        amplitude_out[pres_label] = min(y_data['chamber'])
+
+        # pm.plot_dict(time*1e3, y_data,
+        #              title='',
+        #              xlabel='Time [ms]',
+        #              ylabel='Pressure [#]',
+        #              filename=f'backpressure/{pressure:.4f}')
+
+        print(f'{(i + 1) / len(external_pressure) * 100 :.2f} %')
+
     pm.plot_dict(time*1e3, chamber_pressure,
                  title='',
                  xlabel='Time [ms]',
+                 ylabel='Pressure [#]',
+                 filename=f'backpressure/backpressure_sweep')
+
+    print(amplitude_in)
+    print(amplitude_out)
+
+    amplitude = dict()
+    amplitude['Pr in'] = list(amplitude_in.values())
+    amplitude['Pr out'] = list(amplitude_out.values())
+
+
+    pm.plot_dict(list(amplitude_in.keys()), amplitude,
+                 title='',
+                 xlabel='External pressure [kPa]',
                  ylabel='Chamber pressure [#]',
                  filename=f'backpressure/backpressure_sweep')
 
